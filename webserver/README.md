eedef6d6857467a08ebdab5252284aa08826391b
Moses Mulwa <mosesmulwa123@gmail.com>
2022-03-22 12:35:23 +0300

    finished webserver

--- webserver/README.md
+++ webserver/README.md
@@ -1,4 +1,4 @@
-## C-contiguous Arrat
+## C-contiguous Array
 A contiguous array is just an array stored in an unbroken block of memory: to access the next value in the array, we just move to the next memory address.
 
 Consider the 2D array <br>
@@ -7,3 +7,16 @@ arr = np.arange(12).reshape(3,4). <br> It looks like this: <br>
 In the computer's memory, the values of arr are stored like this: <br>
 ![Contiguous 2](contiguous_2.PNG)<br>
 This means arr is a C contiguous array because the rows are stored as contiguous blocks of memory. The next memory address holds the next row value on that row. If we want to move down a column, we just need to jump over three blocks (e.g. to jump from 0 to 4 means we skip over 1,2 and 3).
+
+
+## Redis Rpush
+Redis RPUSH command inserts all the specified values at the tail of the list stored at the key. If the key does not exist, it is created as an empty list before performing the push operation. When the key holds a value that is not a list, an error is returned.
+
+It is possible to push multiple elements using a single command call just specifying multiple arguments at the end of the command. Elements are inserted one after the other to the tail of the list, from the leftmost element to the rightmost element. So for instance the command RPUSH mylist a b c will result into a list containing a as first element, b as second element and c as third element.
+
+## ------------------------loads and dumps---------------------
+## json.dumps()
+json.dumps() function converts a Python object into a json string.
+
+## json.loads()
+json.loads() method can be used to parse a valid JSON string and convert it into a Python Dictionary. It is mainly used for deserializing native string, byte, or byte array which consists of JSON data into Python Dictionary.
--- webserver/main.py
+++ webserver/main.py
@@ -77,6 +77,56 @@ def predict(request: Request, img_file: bytes=File(...)):
         image_height = int(os.environ.get("IMAGE_HEIGHT"))
         image = prepare_image(image, (image_width, image_height))
         # image is now a numpy array
-        # we need to ensure it is C-contigious otherwise we cannot serialize it.
-        # contigious is explained in the readme
+        # we need to ensure it is C-contiguous otherwise we cannot serialize it.
+        # contiguous is explained in the readme
         image = image.copy(order="C")
+
+
+        # --------------Pushing To redis--------------------
+        # generate an id for the classification 
+        # A UUID (Universal Unique Identifier) is a 128-bit value used to 
+        # uniquely identify an object or entity on the internet
+        # we will use version 4
+        k = str(uuid.uuid4())
+        #encode image to base 64. Serialization
+        image = base64.encode(image).decode('utf-8')
+        #create new  dict instance containing both image and its unique id
+        d = {"id" : k, "image": image}
+        #push d to queue
+        # we are basically saying push d string to our queue called image_queue
+        #queue name is an environment variable which we get
+        #rpush and json dumps are in readme
+        db.rpush(os.environ.get("IMAGE_QUEUE"), json.dumps(d))
+        #------------------check for output-------------
+
+        num_tries = 0 
+        while num_tries < CLIENT_MAX_TRIES:
+            num_tries += 1
+
+            # attempt to grab the output predictions
+            # we use our key which the id called k
+            output = db.get(k)
+
+            #check to see if our model classified the input image
+
+            if output is not None:
+                # add output to data dictionary to be returned to the client
+                output = output.decode('utf-8')
+                #json loads is in readme
+                data["predictions"] = json.loads(output)
+
+                # delete the result from the database and break from the polling loop
+                db.delete(k)
+                break
+        
+            # sleep for a small amount to give the model time to classify the input image
+            time.sleep(float(os.environ.get("CLIENT_SLEEP")))
+
+            # indicate that the request was a success
+            data["success"] = True
+        # we have tried maximum times
+        else:
+            raise HTTPException(status_code=400, detail="Request failed after {} tries".format(CLIENT_MAX_TRIES))
+
+        # return the data dictionary as a json response
+        return data
