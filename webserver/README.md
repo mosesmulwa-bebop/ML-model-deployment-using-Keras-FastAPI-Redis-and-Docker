1dc08107d90ee7e6bfd467c3cf04ed4d8d738e8f
Moses Mulwa <mosesmulwa123@gmail.com>
2022-03-22 11:09:45 +0300

    updated webserver

---
+++ webserver/README.md
@@ -0,0 +1,9 @@
+## C-contiguous Arrat
+A contiguous array is just an array stored in an unbroken block of memory: to access the next value in the array, we just move to the next memory address.
+
+Consider the 2D array <br>
+arr = np.arange(12).reshape(3,4). <br> It looks like this:
+![Contiguous](contiguous_1.PNG)
+In the computer's memory, the values of arr are stored like this:
+![Contiguous 2](contiguous_2.PNG)
+This means arr is a C contiguous array because the rows are stored as contiguous blocks of memory. The next memory address holds the next row value on that row. If we want to move down a column, we just need to jump over three blocks (e.g. to jump from 0 to 4 means we skip over 1,2 and 3).
--- webserver/main.py
+++ webserver/main.py
@@ -35,6 +35,7 @@ CLIENT_MAX_TRIES = int(os.environ.get("CLIENT_MAX_TRIES"))
 
 
 # ---------------------------PREPARE IMAGE-------------------------------
+# Called to all images we receive so they match a specific format for our model
 def prepare_image(image, target):
     """Resize image to desired target(height and width) and pre-process it.
        image here is a PIL Image Instance thus has all the methods of a PIL Image Instance 
@@ -54,3 +55,28 @@ def prepare_image(image, target):
 
     # Return the processed image
     return image
+
+
+# create our home route
+@app.get('/')
+def index():
+    return "Hello world"
+
+#------------------predict route--------------------
+@app.post('/predict')
+def predict(request: Request, img_file: bytes=File(...)):
+    data = {"success":False} # create a new dict called data to be returned
+
+    #we then ensure that an image was properly uploaded to the endpoint
+    if request.method == "POST":
+        # data can be kept as bytes in an in-memory buffer when we use the io module’s Byte IO operations.
+        image = Image.open(io.BytesIO(img_file))
+        # send the image to be prepared
+        #notice that the width and height come from the system variables
+        image_width = int(os.environ.get("IMAGE_WIDTH"))
+        image_height = int(os.environ.get("IMAGE_HEIGHT"))
+        image = prepare_image(image, (image_width, image_height))
+        # image is now a numpy array
+        # we need to ensure it is C-contigious otherwise we cannot serialize it.
+        # contigious is explained in the readme
+        image = image.copy(order="C")
